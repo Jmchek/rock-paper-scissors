5c82d6330774d766acf8182fe0758247002abcd4
Joe Chek <jmchek@gmail.com>
2022-11-09 15:25:06 -0800

    add playRound() function to rps.js

--- rps.js
+++ rps.js
@@ -1,5 +1,3 @@
-console.log("Hello there");
-
 function getComputerChoice() {
     //randomly return Rock, Paper, or Scissors
     let randomNum = Math.floor(Math.random() * 100);
@@ -18,3 +16,28 @@ console.log(getComputerChoice());
 // if the random number is 1-33 return rock
 // if 34 - 66 return Paper
 // if 66-99 return scissors
+
+// Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+// Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
+
+//The function should take two parameters - the playerSelection and computerSelection
+//and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+// Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
+
+// prompt player for choice
+
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (computerSelection == "Paper") {
+        return "You Lose! Paper beats Rock";
+    } else if (computerSelection == "Scissors") {
+        return "You Win! Rock beats Scissors"
+    } else {
+        return "Tied! Try again";
+    }
+  }
+   
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
