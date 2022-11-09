2de83cf654c1c69e272f50f812b3973431d0a447
Joe Chek <jmchek@gmail.com>
2022-11-09 15:00:15 -0800

    add getComputerChoice() function to rps.js

--- rps.js
+++ rps.js
@@ -1,1 +1,20 @@
 console.log("Hello there");
+
+function getComputerChoice() {
+    //randomly return Rock, Paper, or Scissors
+    let randomNum = Math.floor(Math.random() * 100);
+    if (randomNum <= 33) {
+        return "Rock";
+    } else if (randomNum > 33 && randomNum <= 66) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+
+console.log(getComputerChoice());
+
+// random number
+// if the random number is 1-33 return rock
+// if 34 - 66 return Paper
+// if 66-99 return scissors
